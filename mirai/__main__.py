3791c2becab8d56906884585624645dd4d5cba93
Natsu Tadama <nattadasu@nattadasu.my.id>
2024-04-16 17:13:16 +0700

    Format entrypoint script

--- mirai/__main__.py
+++ mirai/__main__.py
@@ -1,12 +1,12 @@
+import traceback
 from sys import exit as sysexit
 from time import time
 
-from librensetsu import convert_float_to_time
-
-from consts import pprint, Status
-from loops import do_loop
+from consts import Status, pprint
 from download import download_database
-import traceback
+from librensetsu.humanclock import convert_float_to_time
+from loops import do_loop
+
 
 def main() -> None:
     """Main process of the util"""
@@ -16,14 +16,19 @@ def main() -> None:
             raise Exception("Failed to download database")
         do_loop()
         end = time()
-        pprint.print(Status.INFO, f'Time elapsed: {convert_float_to_time(end - start)}')
+        pprint.print(
+            Status.INFO, f"Time elapsed: {convert_float_to_time(end - start)}"
+        )
         sysexit(0)
     except Exception as e:
-        pprint.print(Status.ERR, f'An error occurred: {e}')
+        pprint.print(Status.ERR, f"An error occurred: {e}")
         traceback.print_exc()
         end = time()
-        pprint.print(Status.INFO, f'Time elapsed: {convert_float_to_time(end - start)}')
+        pprint.print(
+            Status.INFO, f"Time elapsed: {convert_float_to_time(end - start)}"
+        )
         sysexit(1)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
